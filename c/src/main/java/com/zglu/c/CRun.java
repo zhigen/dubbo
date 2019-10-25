d95990ce366ffffcb133b32f49f121f887fef118
luzhigen <123456>
2019-10-25 09:30:50 +0800

    dubbo 集成 spring boot 测试

--- c/src/main/java/com/zglu/c/CRun.java
+++ c/src/main/java/com/zglu/c/CRun.java
@@ -1,19 +1,12 @@
 package com.zglu.c;
 
-import com.zglu.api.TestService;
-import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CRun {
 
-
     public static void main(String[] args) {
         SpringApplication.run(CRun.class);
     }
-
 }
